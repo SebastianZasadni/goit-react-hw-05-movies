3716c9c9a04b644695d1345b8408f71e82cbaf93
SebastianZasadni <emblelele@gmail.com>
2023-06-21 08:29:10 +0200

    fix

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { fetchMoviesByQuery } from 'api/fetchMovies';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 
 const Movies = () => {
